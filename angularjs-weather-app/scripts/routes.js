f164b958421ab01c55258fb2fe89428dba36caaf
Oreste Luci <oluci@oneboxtm.com>
2015-08-03 18:04:10 +0200

    Added IIFE

--- angularjs-weather-app/scripts/routes.js
+++ angularjs-weather-app/scripts/routes.js
@@ -1,21 +1,25 @@
-// ROUTES
-weatherApp.config(['$routeProvider', function($routeProvider) {
+(function() {
     
-    $routeProvider
-    
-    .when("/", {
-        templateUrl: 'pages/home.html',
-        controller: 'homeController'
-    }) 
-    
-    .when("/forecast", {
-        templateUrl: 'pages/forecast.html',
-        controller: 'forecastController'
-    }) 
+    // ROUTES
+    angular.module(angular_application_name).config(['$routeProvider', function($routeProvider) {
 
-        .when("/forecast/:days", {
-        templateUrl: 'pages/forecast.html',
-        controller: 'forecastController'
-    }) 
+        $routeProvider
 
-}]);
+        .when("/", {
+            templateUrl: 'pages/home.html',
+            controller: 'homeController'
+        }) 
+
+        .when("/forecast", {
+            templateUrl: 'pages/forecast.html',
+            controller: 'forecastController'
+        }) 
+
+            .when("/forecast/:days", {
+            templateUrl: 'pages/forecast.html',
+            controller: 'forecastController'
+        }) 
+
+    }]);
+    
+}());
