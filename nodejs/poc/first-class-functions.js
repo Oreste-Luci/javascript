ee081462f88c64becd79dc3ac9a4e349e539fffe
Oreste Luci <oreste@luci.cl>
2015-12-31 10:02:36 +0100

    Add first class functions example

---
+++ nodejs/poc/first-class-functions.js
@@ -0,0 +1,24 @@
+// Function Statement 
+function greet() {
+    console.log('hi');
+}
+greet();
+
+// Functions are first-class
+function logGreeting(fn) {
+    fn();
+}
+logGreeting(greet);
+
+var greetMe = function() {
+    console.log('Hello');
+}
+greetMe();
+
+// it's first class
+logGreeting(greetMe);
+
+// use function expresion on the fly
+logGreeting(function() {
+    console.log('Hello There');
+});
