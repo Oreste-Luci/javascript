d6bbeda6e9d2952ffc67545971861334b3a0743a
Oreste Luci <oreste@luci.cl>
2016-02-08 22:57:39 +0100

    Add web server examples

---
+++ nodejs/poc/webserver1/app3.js
@@ -0,0 +1,16 @@
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function(req, res){
+    
+    res.writeHead(200, { 'Content-Type': 'text/html'});
+    
+    var html = fs.readFileSync(__dirname + '/index3.html', 'utf8');
+    
+    var message = 'Hello World....';
+    
+    html = html.replace('{message}', message);
+    
+    res.end(html);
+    
+}).listen(1337, '127.0.0.1');
