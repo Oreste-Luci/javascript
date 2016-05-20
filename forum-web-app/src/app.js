ae4dc8f7928cd665db8bcdfa1e5ba301054a7bd5
Oreste Luci <oreste.luci@gmail.com>
2016-05-20 18:13:00 +0200

    Sample ES2015 Web App

---
+++ forum-web-app/src/app.js
@@ -0,0 +1,15 @@
+import Post from "./post";
+import ui from "./ui";
+import User from "./user";
+
+Post.findAll()
+  .then(ui.renderPosts)
+  .catch( (error) => {
+    console.log(error);
+  });
+
+User.findRecent()
+  .then(ui.renderUsers)
+  .catch( (error) => {
+    console.log(error);
+  });
